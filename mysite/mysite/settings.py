9a4fdc50cd11902322cff18f62e07a48dfb72261
Lone Hilke <lonehilke@gmail.com>
2022-06-17 23:42:44 +0200

    prøvet deploy
    prøvet deploy
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +127,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = '*'
+
+django_heroku.settings(locals())
